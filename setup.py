7dc012eb12a4b26b72d9cc59ef5c2bcbff6d9008
tell-k <ffk2005@gmail.com>
2016-11-04 22:36:31 +0900

    Add Djagno1.10 to classfiers in setup.py

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ classifiers = [
     'Framework :: Django :: 1.7',
     'Framework :: Django :: 1.8',
     'Framework :: Django :: 1.9',
+    'Framework :: Django :: 1.10',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
